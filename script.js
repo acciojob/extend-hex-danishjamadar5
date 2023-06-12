41f49f8dfa23d58fe9ed7bf2659fe6ffe69d59fe
danishjamadar5 <117918682+danishjamadar5@users.noreply.github.com>
2023-06-12 17:06:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const extendHex = (shortHex) => {
-  const hex = shortHex.replace('#', '').toLowerCase();
-  const colorComponents = hex.split('');
-  const fullHex = colorComponents
-    .map((component) => component.repeat(2))
-    .join('');
-
-  return `#${fullHex}`;
-};
-
-// Testing the function
+const extendedHex=shortHex=> '#'+(shortHex.slice(shortHex.startsWith('#') ? 1:0).split('').map((curEle) =>curEle+curEle).join(''));
 const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+alert(extendedHex(shortHex));
+
+
+// const extendHex = (shortHex) => {
+//   const hex = shortHex.replace('#', '').toLowerCase();
+//   const colorComponents = hex.split('');
+//   const fullHex = colorComponents
+//     .map((component) => component.repeat(2))
+//     .join('');
+
+//   return `#${fullHex}`;
+// };
+
+// // Testing the function
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
